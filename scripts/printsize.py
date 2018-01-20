b38a923e66cbb8599244174f256b0fae1edca7bc
ArturTan <artur.tanona@gmail.com>
2018-01-20 12:58:48 +0100

    Polishing

--- scripts/printsize.py
+++ scripts/printsize.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 
 def print_size():
     print("{:>18} {:>18}".format("File", "Size(MB)"))
@@ -13,5 +13,17 @@ def print_size():
                 directory = j
 
 
+def script_information():
+    py = re.compile(".py$")
+    print("{:>18}".format("File"))
+    for i in os.walk("."):
+        for j in i:
+            if isinstance(j, list) and j:
+                for file in j:
+                    if py.search(file):
+                        print("{:>18}".format(file))
+
+
 if __name__ == "__main__":
     print_size()
+    script_information()
